fa1395ba225362b196403dce0a2bedaea7beb525
Evan Montano <emontano7096@gmail.com>
2025-07-18 15:06:34 -0700

    Added proeprties to the DataUpdate class. Added a text file with my plan for structuring the training data.

--- TradingViewWebSocket/DataUpdate.cs
+++ TradingViewWebSocket/DataUpdate.cs
@@ -12,12 +12,50 @@ namespace TradingViewWebSocket
     /// </summary>
     public class DataUpdate
     {
+        #region Hard Data
         public string Timestamp { get; set; }
         public string Open { get; set; }
         public string High { get; set; }
         public string Low { get; set; }
         public string Close { get; set; }
         public string Volume { get; set; }
+        #endregion Hard Data
+
+        #region Calculated Data
+        /// <summary>
+        /// Close - Previous Close
+        /// Indicates price movement
+        /// </summary>
+        public string Delta { get; set; }
+
+        /// <summary>
+        /// Close / (Previous Close - 1)
+        /// Normalized signal of trend strength
+        /// </summary>
+        public string PercentChange { get; set; }
+
+        /// <summary>
+        /// High - max(Open, Close)
+        /// </summary>
+        public string TopWick { get; }
+
+        /// <summary>
+        /// min(Open, Close) - Low
+        /// </summary>
+        public string BottomWick { get; }
+
+        /// <summary>
+        /// Absolute difference between the Open and Close
+        /// </summary>
+        public string CandleBodySize { get; }
+
+        /// <summary>
+        /// Time since market open
+        /// </summary>
+        public string MinutesSinceMarketOpen { get; }
+        #endregion Calculated Data
+
+
         public DataUpdate() { }
 
 
